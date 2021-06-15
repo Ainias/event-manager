f375e6e9d924243ab7687ad9a259b546f7c5c606
silas <silas@silas.link>
2021-06-15 11:16:27 +0200

    version update

--- dist/shared/EventManager.js
+++ dist/shared/EventManager.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.EventManager = void 0;
 const js_helper_1 = require("js-helper");
 class EventManager {
     constructor() {
